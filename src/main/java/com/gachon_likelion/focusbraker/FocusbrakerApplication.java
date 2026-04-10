c1afcb98ae0b2edf29454a323f0b9b7663c62dce
SUNG HYUNJOON <tjdguswns1941@gachon.ac.kr>
2026-04-10 20:58:27 +0900

    first commit

---
+++ src/main/java/com/gachon_likelion/focusbraker/FocusbrakerApplication.java
@@ -0,0 +1,13 @@
+package com.gachon_likelion.focusbraker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FocusbrakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FocusbrakerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gachon_likelion/focusbraker/FocusbrakerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gachon_likelion.focusbraker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FocusbrakerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
